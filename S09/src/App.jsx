986bf8586c7e43ca8167a0c0cf857f62661a498b
Kabir <sagithedeveloper@gmail.com>
2026-05-24 13:07:09 +0530

    Added

---
+++ S09/src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+import Header from "./components/header/Header.jsx";
+import Banner from "./components/banner/Banner.jsx";
+import Footer from "./components/footer/Footer.jsx";
+function App() {
+  return (
+    <div>
+      <Header />
+      <Banner />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
